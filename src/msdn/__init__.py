b30b4d45c9a25cd50f763b9275d4296b326df5a2
Ashley Bedford <42564112+abedford37@users.noreply.github.com>
2026-07-01 11:01:16 -0500

    Add export functions to __all__ in init.py

--- src/msdn/__init__.py
+++ src/msdn/__init__.py
@@ -5,5 +5,7 @@ from .catalog import generate
 from .orchestrator import run_pipeline
 from .state import MSDNState
 from .report import build_pipeline_figure, print_summary
+from .export import build_decision_record, export_decision_record
 
-__all__ = ["generate", "run_pipeline", "MSDNState", "build_pipeline_figure", "print_summary"]
+__all__ = ["generate", "run_pipeline", "MSDNState", "build_pipeline_figure",
+           "print_summary", "build_decision_record", "export_decision_record"]
